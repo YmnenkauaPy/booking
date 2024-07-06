811294639d7c7a34c1100dcf83192b48743ea376
YmnenkauaPy <ymnenkaua@gmail.com>
2024-07-06 15:22:00 +0300

    1

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = {
-    path('', rooms_list, name = 'home')
-}
+urlpatterns = [
+    path('', rooms_list, name = 'home'),
+    path('booking_details/<int:pk>/', booking_details, name = 'booking-details'),
+    path('book-room', book_room, name = 'book-room'),
+    path('room_details/<int:pk>/', room_details, name = 'room-details'),
+]
--- reservation/views.py
+++ reservation/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from reservation.models import *
+from django.http import HttpResponse
+
 
 def rooms_list(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
 
-    return render(request=request, template_name = 'reservation/room_list.html', context=context)
+    return render(request=request, template_name = 'reservation/rooms_list.html', context=context)
+
+def booking_details(request, pk):
+    booking = Booking.objects.get(id=pk)
+
+    return render(request, "reservation/booking_details.html", context={"booking":booking})
+
+def room_details(request, pk):
+    room = Room.objects.get(id=pk)
+
+    return render(request, "reservation/room_details.html", context={"room":room})
+
+def book_room(request):
+    if request.method == 'POST':
+        room_number = request.POST.get("room-number")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            room = Room.objects.get(number=room_number)
+        except ValueError:
+            return HttpResponse('Wrong room number', status=400)
+        except Room.DoesNotExist:
+            return HttpResponse('Room does not exist', status=404)
+        
+        booking = Booking.objects.create(user=request.user, room=room, start_time=start_time, end_time=end_time)
+
+        return redirect("booking-details", pk=booking.id)
+
+
+    else:
+        return render(request, "reservation/booking_form.html")
